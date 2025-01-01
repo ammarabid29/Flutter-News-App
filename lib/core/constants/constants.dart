658525f2c92ab73a55dad3192336063921dec29c
ammarabid29 <aammarawan786@gmail.com>
2025-01-01 13:35:01 +0500

    Added packages

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,4 @@
+const String newsAPIBaseURL = "https://newsapi.org/v2";
+const String newsAPIKey = "ed82c7f6f9e34d18a29c604bf4abe816";
+const String countryQuery = "us";
+const String categoryQuery = "general";
